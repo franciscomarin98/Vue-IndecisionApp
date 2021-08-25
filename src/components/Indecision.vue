d52857a14b7182f8d50d09f7a2a51ea8861cf7ff
franciscomarin98 <francmarinc@gmail.com>
2021-08-25 17:11:12 -0500

    Add Indecision Component

---
+++ src/components/Indecision.vue
@@ -0,0 +1,65 @@
+<template>
+  <img src="http://via.placeholder.com/250" alt="bg" />
+  <div class="bg-dark"></div>
+  <div class="indecision-container">
+    <input type="text" placeholder="Hazme una pregunta" />
+    <p>Recuerda terminar con un signo de interrogación (?)</p>
+    <div>
+      <h1>Seré millonario?</h1>
+      <h2>Si, No, ...Pensando</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Indecision",
+};
+</script>
+
+<style scoped>
+img,
+.bg-dark {
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+}
+
+.bg-dark {
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision-container {
+  position: relative;
+  z-index: 99;
+}
+
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+input:focus {
+  outline: none;
+}
+
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
+}
+
+h1,
+h2 {
+  color: white;
+}
+
+h2 {
+  margin-top: 150px;
+}
+</style>
